6336664204f1bbee3a6b8ff10ee9d05dfcf0b6ca
Akarsh1399 <mashalakarsh@gmail.com>
2023-12-21 23:11:54 +0530

    Added Validation for contact page for submit, Added Project page, Added Script Page

---
+++ script.js
@@ -0,0 +1,19 @@
+// let forms = document.getElementById('form');
+
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault()
+
+//     var fname = document.getElementById('fname').value;
+//     console.log(fname)
+// })
+
+function myFunction() {
+    console.log("Pooja")
+    if(fname === " "){
+        alert("Please enter the details")
+    }
+    else {
+        alert("Thank You For Contacting US")        
+    }
+  }
+
