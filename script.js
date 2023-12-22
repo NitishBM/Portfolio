83f288b7fbb8640a8b76502e6bea7238ee966876
Akarsh1399 <mashalakarsh@gmail.com>
2023-12-22 13:06:05 +0530

    Changed the UI of the contact page and, Changed the script code and added styles for contact page

--- script.js
+++ script.js
@@ -7,13 +7,17 @@
 //     console.log(fname)
 // })
 
-function myFunction() {
-    console.log("Pooja")
-    if(fname === " "){
-        alert("Please enter the details")
-    }
-    else {
-        alert("Thank You For Contacting US")        
-    }
-  }
+// function myFunction() {
+//     console.log("Nitish")
+//     if(fname === " "){
+//         alert("Please enter the details")
+//     }
+//     else {
+//         alert("Thank You For Contacting US")        
+//     }
+//   }
 
+function submitbtn() {
+    console.log("nitish");
+    alert("Thank You For Contacting US, We will get back you")
+}
